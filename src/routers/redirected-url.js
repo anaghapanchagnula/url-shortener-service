6c0d533157737339ef6a88c2c73e896463b385c1
Anagha Panchagnula <anaghapanchagnula@anaghas-mbp.lan>
2022-07-07 11:30:01 -0700

    new method

--- src/routers/redirected-url.js
+++ src/routers/redirected-url.js
@@ -3,22 +3,21 @@ const router = express.Router();
 const Url = require('../models/urlmodel');
 
 router.get('/:urlHash', async (req, res) => {
-  const storedUrlsArray = await Url.find({});
-  const enteredUrl = await Url.findOne({ urlHash: req.params.urlHash });
+  const enteredUrl = await Url.findOneAndUpdate(
+    {
+      urlHash: req.params.urlHash,
+    },
+    { $inc: { clicks: 1 } }
+  );
 
   try {
-    storedUrlsArray.forEach((url) => {
-      if (enteredUrl.url === url.url) {
-        enteredUrl.clicks++;
-        enteredUrl.save();
-        res.set('Content-Type', 'application/json');
-        res
-          .status(302)
-          .send(
-            `Location: ${enteredUrl.url}\n Number of clicks: ${enteredUrl.clicks}`
-          );
-      }
-    });
+    await enteredUrl.save();
+    res.set('Content-Type', 'application/json');
+    res
+      .status(302)
+      .send(
+        `Location: ${enteredUrl.url}\n Number of clicks: ${enteredUrl.clicks}`
+      );
   } catch (error) {
     res.status(404).send(error);
   }
